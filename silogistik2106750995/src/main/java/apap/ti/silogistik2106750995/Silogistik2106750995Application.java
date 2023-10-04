b9f5f9d1ea2bcf3da100883a3988351aaa36f9c5
rionadani <rionadani@gmail.com>
2023-10-04 10:11:50 +0700

    initial commit

---
+++ silogistik2106750995/src/main/java/apap/ti/silogistik2106750995/Silogistik2106750995Application.java
@@ -0,0 +1,13 @@
+package apap.ti.silogistik2106750995;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Silogistik2106750995Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Silogistik2106750995Application.class, args);
+	}
+
+}
